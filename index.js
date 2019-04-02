63279cfb9b9b5da54b91ba591fb9e028b2e87b7f
JingChen <jingchenjc2019@gmail.com>
2019-04-02 01:40:27 -0500

    Done.

--- index.js
+++ index.js
@@ -3,9 +3,22 @@
 function handleClick(e) {
   const timeString = document.getElementById('time').value
   displayMessage(greet(timeString))
-}
+};
 
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(string){
+  let hr = string.split(":")[0];
+  if (hr < 12){
+    return "Good Morning";
+  } else if (hr > 12 && hr < 17){
+    return "Good Afternoon";
+  } else if (hr > 17){
+    return "Good Evening";
+  }
+};
 /* Write your implementation of displayMessage() */
+function displayMessage(string){
+  document.querySelector("#greeting").innerHTML = string
+};
