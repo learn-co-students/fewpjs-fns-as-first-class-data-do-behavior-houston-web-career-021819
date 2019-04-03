0a6b81cc2f6982a2f122a3d2782e17cef48112fe
github username <github email address>
2019-04-03 08:52:37 -0500

    Done.

--- index.js
+++ index.js
@@ -5,6 +5,26 @@ function handleClick(e) {
   displayMessage(greet(timeString))
 }
 
+function greet(timeString) {
+  let hour = parseInt(timeString.split(":"))
+
+  if (hour < 12) {
+    return `Good Morning`
+  }
+
+  else if (hour > 12 && hour < 17) {
+    return `Good Afternoon`
+  }
+
+  else {
+    return `Good Evening`
+  }
+}
+
+function displayMessage(greet) {
+  let message = document.querySelector('#greeting')
+  message.innerHTML = greet
+}
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
